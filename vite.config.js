733ed4c3837758e179db23643ccba76d48db48b4
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-07-20 00:47:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    dedupe: ['clsx'], // <-- Helps with resolving from nested deps
+  },
   build: {
     rollupOptions: {
-      external: ['@tabler/icons-react'],
+      external: ['clsx'], // <-- Added clsx
     },
   },
 })
